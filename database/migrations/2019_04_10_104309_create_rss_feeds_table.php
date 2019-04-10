c43dc389016872908f492531186e2aecdb6a85d6
Jian Han <jianhan@mail.com>
2019-04-10 20:56:33 +1000

    [feat] added model and migration

---
+++ database/migrations/2019_04_10_104309_create_rss_feeds_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRssFeedsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rss_feeds', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('url', 256);
+            $table->string('name', 256);
+            $table->string('site_url', 256);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rss_feeds');
+    }
+}
