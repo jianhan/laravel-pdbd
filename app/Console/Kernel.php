ccfac15079ce382bf5aa4bc2441d9ceb34a12e68
Jian Han <jianhan@mail.com>
2019-04-20 22:25:30 +1000

    [feat] remove sample tests, refactor queue

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Jobs\SyncFeeds;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,8 +25,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $feeds = \App\Models\Feed::isActive()->notFetchedYet()->get();
+        foreach ($feeds as $feed) {
+            $schedule->job(new SyncFeeds($feed), 'scrape')->everyMinute();
+        }
     }
 
     /**
@@ -35,7 +38,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
--- app/Jobs/SyncFeeds.php
+++ app/Jobs/SyncFeeds.php
@@ -7,7 +7,6 @@ use App\FeedSyncers\Syncer;
 use App\Models\Feed;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
@@ -16,16 +15,16 @@ class SyncFeeds implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    private $feeds;
+    private $feed;
 
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct(Collection $feeds)
+    public function __construct(Feed $feed)
     {
-        $this->feeds = $feeds;
+        $this->feed = $feed;
     }
 
     /**
@@ -35,9 +34,6 @@ class SyncFeeds implements ShouldQueue
      */
     public function handle()
     {
-        $this->feeds->map(function (Feed $feed) {
-            $syncer = new Syncer($feed, Reader::getInstance($feed->link));
-            $syncer->sync();
-        });
+        (new Syncer($this->feed, Reader::getInstance($this->feed->link)))->sync();
     }
 }
